8e3ee164c1c3cbe6a0c8465833f6df2189b6cb68
Jared Hoberock <jaredhoberock@gmail.com>
2016-10-28 20:11:53 -0500

    Introduce unit_hemisphere_distribution

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,10 @@
-#include "distribution2d/unit_sphere_distribution.hpp"
+#include "distribution2d/unit_hemisphere_distribution.hpp"
 #include <random>
 #include <cmath>
 #include <cassert>
 #include <iostream>
 
-bool almost_equal(float x, float y, float epsilon = 0.00001f)
+bool almost_equal(float x, float y, float epsilon = 0.001f)
 {
   float relative_difference = std::fabs((x - y) / x);
   return relative_difference <= epsilon;
@@ -14,10 +14,10 @@ int main()
 {
   std::default_random_engine rng;
 
-  dist2d::unit_sphere_distribution<> dist;
+  dist2d::unit_hemisphere_distribution<> dist;
 
   // compute a Monte Carlo estimate of the area of the distribution 
-  size_t n = 1;
+  size_t n = 10000;
   float estimate = 0;
   for(int i = 0; i < n; ++i)
   {
---
+++ distribution2d/unit_hemisphere_distribution.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "unit_square_distribution.hpp"
+#include <tuple>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <type_traits>
+
+namespace dist2d
+{
+
+
+// a uniform distribution of points on the unit hemisphere
+template<class Point = std::tuple<float,float,float>>
+class unit_hemisphere_distribution
+{
+  public:
+    using result_type = Point;
+
+  private:
+    using real_type1 = typename std::tuple_element<0,result_type>::type;
+    using real_type2 = typename std::tuple_element<1,result_type>::type;
+    using real_type3 = typename std::tuple_element<2,result_type>::type;
+
+  public:
+    using real_type = typename std::common_type<real_type1, real_type2, real_type3>::type;
+
+  private:
+    static constexpr real_type pi = 3.14159265;
+    static constexpr real_type two_pi = real_type(2) * pi;
+
+  public:
+    template<class Integer1, class Integer2>
+    result_type operator()(Integer1 urn1, Integer2 urn2) const
+    {
+      unit_square_distribution<std::pair<real_type,real_type>> square;
+
+      real_type u1;
+      real_type u2;
+      std::tie(u1,u2) = square(urn1,urn2);
+
+      real_type3 z = u1;
+      real_type r = std::sqrt(std::max(real_type(0), real_type(1) - z*z));
+      real_type phi = two_pi * u2;
+      real_type1 x = r * std::cos(phi);
+      real_type2 y = r * std::sin(phi);
+
+      return result_type{x,y,z};
+    }
+
+    template<class Integer,
+             class = typename std::enable_if<
+               std::is_integral<Integer>::value
+             >::type>
+    result_type operator()(Integer i) const
+    {
+      auto xy = decode_morton_2d(i);
+      return operator()(xy.first, xy.second);
+    }
+
+    template<class Generator,
+             class = typename std::enable_if<
+               detail::is_integral_generator<Generator>::value
+             >::type>
+    result_type operator()(Generator& g) const
+    {
+      return operator()(g());
+    }
+
+    static bool contains(const result_type& p)
+    {
+      // p must not be in the -z hemisphere
+      if(std::get<2>(p) < 0) return false;
+
+      real_type radius_squared = std::get<0>(p) * std::get<0>(p) + std::get<1>(p) * std::get<1>(p) + std::get<2>(p) * std::get<2>(p);
+
+      return std::fabs(real_type(1) - radius_squared) < 0.000005f;
+    }
+
+    // if !contains(p) the result is undefined
+    constexpr static real_type probability_density(const result_type& p)
+    {
+      return real_type(1) / area();
+    }
+
+    constexpr static real_type area()
+    {
+      return real_type(2) * pi;
+    }
+};
+
+
+} // end dist2d
+
+
